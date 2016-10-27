648f000c8ba9a9f8293b09874b04d4f653f38ce1
Samantha Davis <samantha.shea.davis@gmail.com>
2016-10-27 17:49:23 -0400

    sort function working, all templates forms added

--- src/js/controllers/products.controller.js
+++ src/js/controllers/products.controller.js
@@ -1,5 +1,10 @@
 angular.module('shopularApp').controller('ProductsController', function(inventory, $q) {
+
 var self = this;
+
+this.orderByField = 'totalPrice';
+this.reverseSort = false;
+
   this.allProducts = {};
 
   $q.when(inventory.get()).then(function(response) {
@@ -8,17 +13,12 @@ var self = this;
     self.updatePrice(self.allProducts);
   });
 
-// $scope.orderByField = 'totalPrice';
-// $scope.reverseSort = false;
-
-
 this.updatePrice = function(products) {
     for (var count = 0; count<products.length; count++) {
       var newPrice = ((products[count].price - products[count].discount) * 1.0575);
       products[count].totalPrice = newPrice;
     }
 };
-
 
 this.onSale = function(discount) {
     if (discount > 0) {
